17aaa89a2dfae261ca12738784addaefaadd2167
Katarina <zain.fake02@gmail.com>
2020-09-13 18:08:16 +0200

    ayayaya

--- utils/getUserId.js
+++ utils/getUserId.js
@@ -7,13 +7,3 @@ module.exports = async (account) => {
   const data = await axios.get(url);
   return data.data.id;
 };
-
-// {
-//     "id": "F8W2xvw812eUcL_Ri2J9lZMwcz9WIWtTnYJmIGuH8gEeOjvk",
-//     "accountId": "Zgo6cW4iVbw4s7t1QgRaKZDntFmf1bWhe5aF2jwUGGlLPFkdD5EEfg78",
-//     "puuid": "YebE61kOnFkqvKT0urW99ehG98MDRrJaMEvHT67y5WDuXsPQqwiEXbcF_rkmmhd_2sIwvxTd3Yw09Q",
-//     "name": "zaino02",
-//     "profileIconId": 4248,
-//     "revisionDate": 1599858926000,
-//     "summonerLevel": 202
-// }
