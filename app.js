cb2ddc419c068f49a1653c24e4c134aff4514853
Katarina <zain.fake02@gmail.com>
2020-09-15 18:51:05 +0200

    adding everkers side

--- app.js
+++ app.js
@@ -58,6 +58,48 @@ app.get("/", async (req, res) => {
     });
 });
 
+// everkers accounts
+const everkers_account = [
+  {
+    region: "euw1",
+    name: "salikhwladlqhab",
+  },
+  {
+    region: "euw1",
+    name: "renekmoon",
+  },
+];
+
+app.get("/everkers", async (req, res) => {
+  const f = everkers_account.map(async (account) => {
+    // get user id
+    const id = await getUserId(account);
+
+    //get account champion mastery
+    const champions = await getChampionMastery(id, account.region);
+
+    // filter the champion by they id
+    // kata id: 55 base on the official riot champ ids
+    // http://ddragon.leagueoflegends.com/cdn/10.18.1/data/en_US/champion.json
+    const kata = champions.filter((champ) => champ.championId === 58);
+
+    // and we return the value mastery
+    return kata[0].championPoints;
+  });
+  Promise.all(f)
+    .catch((err) => console.log(err))
+    .then((result) => {
+      // after returning all the mastery poitn from each account
+      // now we add them to each other
+      const mastery = result.reduce((a, b) => {
+        return a + b;
+      }, 0);
+
+      // bingo the total katarina point xD
+      res.status(200).json({ mastery });
+    });
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("omg we are in 5000");
 });
